9eca55c4181101c9bcdf24eb202a39398fa31bab
Row1n <123@cc.com>
2022-04-29 19:04:58 +0800

    for主页编写

--- MiniMarket/MiniMarket/settings.py
+++ MiniMarket/MiniMarket/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
+    'blogs',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,10 +79,10 @@ WSGI_APPLICATION = 'MiniMarket.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django',
-        'USER': 'django',
-        'PASSWORD': 'django',
-        'HOST': 'catlessfish.cc', # 留空表示localhost
+        'NAME': 'minimarket',
+        'USER': 'minimarket',
+        'PASSWORD': 'minimarket',
+        'HOST': 'catlessfish.cc',
         'PORT': '5432',
     }
 }
@@ -119,7 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+import os
+
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    (os.path.join(BASE_DIR, 'static'))
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
