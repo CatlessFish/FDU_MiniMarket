36a913262464380aa332de6e1678ac766be91d80
Row1n <123@cc.com>
2022-04-30 04:31:51 +0800

    123

--- MiniMarket/blogs/models.py
+++ MiniMarket/blogs/models.py
@@ -1,40 +1,4 @@
-<<<<<<< HEAD
 from django.db import models
-from accounts.models import SiteUser
-# Create your models here.
-
-class Record(models.Model):
-    is_want = models.BooleanField(
-        verbose_name="是否是一个需求帖",
-        default=True,
-    )
-    want = models.CharField(
-        max_length=200,
-        verbose_name="想换到的物品描述、数量",
-        blank=False,
-    )
-    offer = models.CharField(
-        max_length=200,
-        verbose_name="可以用于交换的物品描述、数量",
-        blank=False,
-    )
-    note = models.CharField(
-        max_length=300,
-        verbose_name="备注",
-        blank=True,
-    )
-    update_time = models.DateTimeField(
-        auto_now=True,
-    )
-    created_by = models.ForeignKey(
-        SiteUser,
-        on_delete=models.CASCADE,
-    )
-    def __str__(self) -> str:
-        return '想要：' + str(self.want)
-=======
-from django.db import models
-
 from accounts.models import SiteUser
 
 # Create your models here.
@@ -90,4 +54,3 @@ class Subscribe(models.Model):
         max_length=100,
         verbose_name="留言",
     )
->>>>>>> 8aea2fa5b0619c8117d2507ab3eece609c63d4d1
--- MiniMarket/blogs/views.py
+++ MiniMarket/blogs/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import View
 from django import forms
+from accounts.models import SiteUser
 
 from .models import *
 from .forms import *
@@ -61,7 +62,7 @@ class NewWantView(View):
     Create a new Want-Record
     """
     template = 'blogs/new_want.html' # 使用的模板
-    success_redirect = '/' # 成功时重定向地址
+    success_redirect = 'blogs:all_record' # 成功时重定向地址
 
     def get(self, request):
         form = Want_form()
@@ -70,33 +71,34 @@ class NewWantView(View):
             
     def post(self, request):
         form = Want_form(request.POST)
-        if form.is_valid():
-            new_want = form.save()
-            return redirect(self.success_redirect)
-        else:
-            errors = {name:list(form.errors[name]) for name in form.errors}
-            return render(request, self.template_name,
-                context={'form': form, 'errors': errors})
-
+        user_id = form.data.get('user_id')
+        want = form.data.get('want')
+        offer = form.data.get('offer')
+        note = form.data.get('note')
+        user = SiteUser.objects.get(id = user_id)
+        record = Record(is_want=1, want=want, offer=offer, note=note, created_by=user)
+        record.save()
+        return redirect(self.success_redirect)
 
 class NewOfferView(View):
     """
     Create a new Offer-Record
     """
     template = 'blogs/new_offer.html' # 使用的模板
-    success_redirect = '/' # 成功时重定向地址
+    success_redirect = 'blogs:all_record' # 成功时重定向地址
 
     def get(self, request):
-        form = Offer_form()
+        form = Want_form()
         return render(request, self.template,
             context={'form': form})
             
     def post(self, request):
-        form = Offer_form(request.POST)
-        if form.is_valid():
-            new_want = form.save()
-            return redirect(self.success_redirect)
-        else:
-            errors = {name:list(form.errors[name]) for name in form.errors}
-            return render(request, self.template_name,
-                context={'form': form, 'errors': errors})
+        form = Want_form(request.POST)
+        user_id = form.data.get('user_id')
+        want = form.data.get('want')
+        offer = form.data.get('offer')
+        note = form.data.get('note')
+        user = SiteUser.objects.get(id = user_id)
+        record = Record(is_want=0, want=want, offer=offer, note=note, created_by=user)
+        record.save()
+        return redirect(self.success_redirect)
