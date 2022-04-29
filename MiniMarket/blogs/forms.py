3b0c28560f0c38b66e9e55d0ea19640164bb78b0
Li Yuzhi <791517865@qq.com>
2022-04-29 19:47:08 +0800

    Record Model

---
+++ MiniMarket/blogs/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+from django.forms import fields, ModelForm
+from django.core.exceptions import ValidationError
+
+from .models import *
+
+class Want_form(ModelForm):
+    class Meta:
+        model = Record
+        fields = ['want', 'offer', 'note']
+        help_texts = {
+            'want': '你想要啥？',
+            'offer': '你能提供什么？',
+            'note': '备注',
+        }
+
+    def save(self, commit=True):
+        record = super.save(commit=False)
+        record.is_want = True
+        if commit:
+            record.save()
+        return record
+
+
+class Offer_form(ModelForm):
+    class Meta:
+        model = Record
+        fields = ['offer', 'want', 'note']
+        help_texts = {
+            'want': '你想要啥？',
+            'offer': '你能提供什么？',
+            'note': '备注',
+        }
+
+    def save(self, commit=True):
+        record = super.save(commit=False)
+        record.is_want = False
+        if commit:
+            record.save()
+        return record
