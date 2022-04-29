8d2ed985105b55033a4de80b3bcd2adde4d1eaa3
Yuzhi <791517865@qq.com>
2022-04-29 18:19:02 +0800

    1

--- MiniMarket/MiniMarket/settings.py
+++ MiniMarket/MiniMarket/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'MiniMarket.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'catlessfish.cc', # 留空表示localhost
+        'PORT': '5432',
     }
 }
 
